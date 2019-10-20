1d76e67996c8ce0095522905bbfbba6f602279e5
James Willett <jms.wlltt@gmail.com>
2019-10-20 00:35:01 -0400

    its my own applescript, volume thing

--- js/spotify.js
+++ js/spotify.js
@@ -1,4 +1,4 @@
-const spotify = require('spotify-node-applescript');
+const applescript = require('applescript');
 
 const trunc = (s, l) => s.length - 3 < l ? s : s.substring(0, l) + '...';
 
@@ -16,45 +16,52 @@ const timeString = (rawSeconds) => {
   return `${finalHours}${finalMinutes}:${finalSeconds}`;
 };
 
+const v = (num) => {
+  if (num == 0) return '';
+  if (num < 16) return '⠄';
+  if (num < 32) return '⠆';
+  if (num < 48) return '⠇';
+  if (num < 64) return '⠏';
+  if (num < 80) return '⠟';
+  return '⠿';
+};
+
 const p = (fn) => new Promise((resolve, reject) =>
   fn((e, d) => e ? reject(e) : resolve(d)));
 
-(async () => {
-  try {
-    const isRunning = await p(spotify.isRunning)
-    if (!isRunning) return console.log('');
-  } catch (e) {
-    return console.log('')
-  }
-  return Promise.all([
-    p(spotify.getState),
-    p(spotify.getTrack),
-    p(spotify.isShuffling),
-    p(spotify.isRepeating),
-  ])
-    .then(([state, track, isShuffling, repeating]) => {
-      const r = repeating ? ' ⟳' : '';
-      const s = state.state === 'playing' ? '►' : '✘';
-      const shuf = isShuffling ? ' ⤭' : '';
-      const playStates = `[ ${s}${shuf}${r} ]`;
+applescript.execFile('/Users/james/configs/scripts/spotify', (err, d) => {
+  if (err) return;
+  const data = JSON.parse(d);
+  if (!data.running) return;
+  const {
+    shuffling, repeating, state,
+    position, duration,
+    artist, title, volume,
+  } = JSON.parse(d)
 
-      const curr = timeString(state.position);
-      const tot = timeString(Math.floor(track.duration / 1000));
-      const times = `[${curr} - ${tot}]`;
+  const r = repeating ? ' ⟳' : '';
+  const s = state === 'playing' ? '►' : '✘';
+  const shuf = shuffling ? ' ⤭' : '';
+  const playStates = `[ ${s}${shuf}${r} ]`;
 
-      const string = `${playStates} ${track.artist} - ${track.name} ${times}`;
-      const truncString = `${
-        playStates // is this an ugly way to do this?
-      } ${
-        trunc(track.artist, 17) // probably.
-      }${
-        track.artist ? ' - ' : '' // do i care?
-      }${
-        trunc(track.name, track.artist ? 30 : 45) // nope
-      } ${
-        times // sorry
-      }`;
+  const curr = timeString(position);
+  const tot = timeString(Math.floor(duration));
+  const times = `[${curr} - ${tot}]`;
 
-      console.log(`#[fg=colour10,bg=colour240]#[fg=colour232,bg=colour10,bold] ${truncString} `);
-    });
-})()
+  const string = `${playStates} ${artist} - ${title} ${times}`;
+  const truncString = `${
+    playStates // is this an ugly way to do this?
+  } ${
+    trunc(artist, 17) // probably.
+  }${
+    artist ? ' - ' : '' // do i care?
+  }${
+    trunc(title, artist ? 30 : 45) // nope
+  } ${
+    times // sorry
+  } ${
+    v(volume)
+  }`;
+
+  console.log(`#[fg=colour10,bg=colour240]#[fg=colour232,bg=colour10,bold] ${truncString} `);
+});
