1ec6468b43159c507ed77c6fafb330bd82655986
James Willett <jms.wlltt@gmail.com>
2020-01-13 10:27:04 -0500

    turn HOME to ~

--- js/pwd.js
+++ js/pwd.js
@@ -1,12 +1,18 @@
 const longDir = process.argv[2] || 'Users/james.willett/documents/hello.js';
+const branch = process.argv[3];
 
-const shortDir = longDir.replace(/^\//, '').split('/').reduce((a, c, i, arr) => {
-  if (i < arr.length - 1) {
-    c = c[0];
-  }
-  if (i === 0 && c === '~') return c;
-  return `${a}/${c}`;
-}, '');
+const prettyBranch = !branch ? '' : ` #[fg=#ffaa00] ${branch}`;
 
-console.log(shortDir);
+const shortDir = longDir.replace(new RegExp(`^${process.env.HOME}`), '~')
+  .replace(/^\//, '')
+  .split('/')
+  .reduce((a, c, i, arr) => {
+    if (i < arr.length - 1) {
+      c = c[0];
+    }
+    if (i === 0 && c === '~') return c;
+    return `${a}/${c}`;
+  }, '');
+
+console.log(`${shortDir}${prettyBranch}`);
 
