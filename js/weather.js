bf427838a60eacc40d11b0648fe50de9a2d5cc27
James Willett <jms.wlltt@gmail.com>
2020-02-05 10:52:49 -0500

    use minutely darksky stuff

--- js/weather.js
+++ js/weather.js
@@ -61,24 +61,49 @@ const stringForDay = (c, service, fg) =>
   }#[nobold]:#[fg=#ffaa00,bold][#[fg=${fg},nobold]${
     getEmoji(c.code, service)
   }${
-    c.precip ? ` (${c.precip * 100}%)` : ''
+    c.precip ? ` (${Math.floor(c.precip * 100)}%)` : ''
   } ${
     getColor(c.high, fg)
   }℉/${
     getColor(c.low, fg)
   }℉#[fg=#ffaa00,bold]]#[fg=${fg},nobold]`;
 
-const makeString = ({ now, today, tomorrow: t, later: l, extendedForecast }, service) => {
+const makeString = ({ now, today, tomorrow: t, later: l, extendedForecast, minutely }, service) => {
   const rightNow = new Date();
   const showMoon = rightNow < new Date(today.sunrise * 1000) || rightNow > new Date(today.sunset * 1000);
+  const sigPrecip = now.precipIntensity >= 0.1;
+  const nextPrecip = minutely.data.find((m) => {
+    if (sigPrecip) return m.precipIntensity < 0.1;
+    return m.precipIntensity >= 0.1;
+  });
   const bg = 'colour233';
   const fg = '#BBBBBB';
   const main = `#[bg=${bg}] ${getEmoji(now.code, service)} ${getColor(now.temp, fg)}℉`;
   const later = ` -> ${getEmoji(l.code, service)} ${getColor(l.temp, fg)}℉`;
+  const precipString = (() => {
+    if (sigPrecip) {
+      if (nextPrecip) {
+        return ` ${
+          getEmoji(nextPrecip.precipType, service)
+        } stopping in ${
+          new Date(nextPrecip.time) - rightNow
+        }`;
+      }
+      return ' for the hour';
+    }
+    if (nextPrecip) {
+      return ` ${
+        getEmoji(nextPrecip.precipType, service)
+      } in ${
+        new Date(nextPrecip.time) - rightNow
+      }`;
+    }
+    return '';
+  })();
   const moon = showMoon ? ` ${getMoon(today.moonPhase)}` : '';
   const highLow = '  ' + stringForDay(today, service, fg);
   const tomorrow = ' | ' + stringForDay(t, service, fg);
-  const WEATHER = main + later + moon + highLow + tomorrow;
+  const WEATHER = main + later + precipString + moon + highLow + tomorrow;
   const maxExtended = (width - 145) / 13;
   if (width < 100) return WEATHER;
   const extended = extendedForecast.reduce((a, c, i) => {
@@ -109,7 +134,7 @@ if ((now.getSeconds() === 0 || invokeImmediately) && isFirstSession) {
     })
     .then((d) => {
       if (invokeImmediately) console.log('weather: ', d);
-      const { currently, hourly, daily } = d;
+      const { currently, hourly, daily, minutely } = d;
       const later = hourly.data[0];
       const [ today, tomorrow, ...restOfDays] = daily.data;
       const parts = {
@@ -125,6 +150,7 @@ if ((now.getSeconds() === 0 || invokeImmediately) && isFirstSession) {
         },
         today: mapDay(today),
         tomorrow: mapDay(tomorrow),
+        minutely,
         extendedForecast: restOfDays.map(mapDay),
       };
       const string = makeString(parts, 'darksky');
